bf5f8e2096ae1a12b97520bf3bfff53142678649
dominikamac758 <dominika06maciejewska@gmail.com>
2025-12-06 23:16:26 +0100

    Update skoczek.cpp

--- skoczek.cpp
+++ skoczek.cpp
@@ -4,31 +4,37 @@
 
 using namespace std;
 
-int **board;
-int N;
+int **plansza;     
+int N;             
 
-int moveX[8] = { 2, 1, -1, -2, -2, -1, 1, 2 };
-int moveY[8] = { 1, 2,  2,  1, -1, -2, -2, -1 };
+int ruchX[8] = { 2, 1, -1, -2, -2, -1, 1, 2 };
+int ruchY[8] = { 1, 2,  2,  1, -1, -2, -2, -1 };
 
-bool isSafe(int x, int y) {
-    return (x >= 0 && y >= 0 && x < N && y < N && board[x][y] == 0);
+bool bezpiecznePole(int x, int y) {
+    return (x >= 0 && y >= 0 && x < N && y < N && plansza[x][y] == 0);
 }
 
-bool solveKTUtil(int x, int y, int moveCount) {
-    if (moveCount == N * N + 1)
+bool znajdzTrase(int x, int y, int numerRuchu) {
+
+    if (numerRuchu == N * N + 1)
         return true;
 
+    
     for (int i = 0; i < 8; i++) {
-        int nextX = x + moveX[i];
-        int nextY = y + moveY[i];
 
-        if (isSafe(nextX, nextY)) {
-            board[nextX][nextY] = moveCount;
+        int nastepnyX = x + ruchX[i];
+        int nastepnyY = y + ruchY[i];
 
-            if (solveKTUtil(nextX, nextY, moveCount + 1))
+        if (bezpiecznePole(nastepnyX, nastepnyY)) {
+
+            plansza[nastepnyX][nastepnyY] = numerRuchu;
+
+            
+            if (znajdzTrase(nastepnyX, nastepnyY, numerRuchu + 1))
                 return true;
 
-            board[nextX][nextY] = 0;
+            
+            plansza[nastepnyX][nastepnyY] = 0;
         }
     }
 
@@ -36,42 +42,42 @@ bool solveKTUtil(int x, int y, int moveCount) {
 }
 
 int main(int argc, char *argv[]) {
+
     if (argc != 4) {
-        cout << "Uzycie: ./skoczek.exe <N> <W> <K>\n";
+        cout << "Uzycie: ./skoczek <N> <wiersz> <kolumna>\n";
         return 1;
     }
 
     N = atoi(argv[1]);
-    int W = atoi(argv[2]) - 1; 
-    int K = atoi(argv[3]) - 1;
-
-
-    board = new int*[N];
-    for (int i = 0; i < N; i++) {
-        board[i] = new int[N];
-        for (int j = 0; j < N; j++)
-            board[i][j] = 0;
-    }
+    int startWiersz = atoi(argv[2]) - 1;  
+    int startKolumna = atoi(argv[3]) - 1; 
 
     
-    board[W][K] = 1;
+    plansza = new int*[N];
+    
+    for (int i = 0; i < N; i++) {
+        plansza[i] = new int[N];
+        for (int j = 0; j < N; j++)
+            plansza[i][j] = 0;
+    }
 
-    if (!solveKTUtil(W, K, 2)) {
+    plansza[startWiersz][startKolumna] = 1;
+
+    if (!znajdzTrase(startWiersz, startKolumna, 2)) {
         cout << "Brak rozwiazania.\n";
         return 1;
     }
 
-    
+
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++)
-            cout << setw(4) << board[i][j] << " ";
+            cout << setw(4) << plansza[i][j] << " ";
         cout << "\n";
     }
 
-    
     for (int i = 0; i < N; i++)
-        delete[] board[i];
-    delete[] board;
+        delete[] plansza[i];
+    delete[] plansza;
 
     return 0;
 }

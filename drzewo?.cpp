3e0ca86865bc996f09c36cda5ae6e92f0be8ea85
dominikamac758 <dominika06maciejewska@gmail.com>
2026-01-12 22:47:33 +0100

    Create drzewo?.cpp

---
+++ drzewo?.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<iomanip>
+#include<string>
+
+using namespace std;
+
+struct Node{
+    
+    char litera;
+    Node*brat=nullptr;
+    Node*dziecko=nullptr;
+};
+
+Node*root=nullptr;
+
+void dodaj(string slowo){
+    
+    int index=0;
+    if(!root){
+        Node*temp=new Node();
+        temp->litera=slowo[0];
+        root=temp;
+        index=1;
+    }
+    
+    Node*current=root;
+
+    
+    while(current->brat!=nullptr){
+        
+        if(current->litera==slowo[index])
+        {
+            index++;
+            current=current->dziecko;
+        }
+    }
+    
+    if(current->brat==nullptr && current!=slowo[index]){
+        
+        Node*nowy=new Node();
+        nowy->litera=slowo[index];
+        
+        current->brat=nowy;
+    
+        
+    }else if(current->brat==nullptr && current==slowo[index]){
+        
+        current=current->dziecko;
+    }
+    
+    
+}
+
+int main(){
+    
+    
+    return 0;
+}
